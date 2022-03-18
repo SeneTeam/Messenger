175cb2c4a3296118ceb570a5ca3bac207642bc51
minhchientnck <minhchientnck@gmail.com>
2022-03-18 21:53:48 +0700

    commit first

---
+++ client/src/components/ActiveChat/Messages.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { SenderBubble, OtherUserBubble } from '.';
+import moment from 'moment';
+
+const Messages = (props) => {
+  const { messages, otherUser, userId } = props;
+
+  const sortedMessages = messages.sort((a,b)=>b.createdAt > a.createdAt ? -1 : b.createdAt < a.createdAt ? 1 : 0);
+
+  return (
+    <Box>
+      {sortedMessages.map((message) => {
+        const time = moment(message.createdAt).format('h:mm');
+
+        return message.senderId === userId ? (
+          <SenderBubble key={message.id} text={message.text} time={time} />
+        ) : (
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+export default Messages;

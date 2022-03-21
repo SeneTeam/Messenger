5c87190b81f041e53ff8946d3f1228cc19f395c1
minhchienef <minhchien.ef@gmail.com>
2022-03-21 12:19:22 +0700

    commit issue01

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import {
+  Container,
   Grid,
   Box,
   Typography,
@@ -8,6 +9,16 @@ import {
   FormControl,
   TextField,
 } from '@material-ui/core';
+import './Login.scss';
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${'bg-img.png'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+  }
+};
 
 const Login = ({ user, login }) => {
   const history = useHistory();
@@ -27,43 +38,55 @@ const Login = ({ user, login }) => {
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Link href="/register" to="/register">
-            <Button>Register</Button>
-          </Link>
+    <Box className="Login-Container" height="100vh" display="flex" flex="1" justifyContent="space-around">
+      <Grid container justifyContent="center">
+        <Grid className="left-column" item xs={5} style={styles.paperContainer} justifyContent="center">
+          <div className="bubble-icon">
+            <img src="bubble.svg" height={80} width={80} />
+            <br />
+            <Typography>Converse with anyone with any language</Typography>
+          </div>
         </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
+        <Grid className="right-column" item xs={7} direction="column">
+          <Container>
+            <Box display="flex" flex="1" justifyContent="flex-end">
+              <Typography>Don't have an account?</Typography>
+              <Link className="link" href="/register" to="/register">
+                <Button variant="contained">Register</Button>
+              </Link>
+            </Box>
+          </Container>
+          <form onSubmit={handleLogin}>
+            <label className="banner">Welcome back!</label>
             <Grid>
+              <Grid>
+                <FormControl margin="normal" required>
+                  <TextField
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                  />
+                </FormControl>
+              </Grid>
               <FormControl margin="normal" required>
                 <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
+                  label="password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
                 />
               </FormControl>
+              <Grid container justifyContent="center">
+                <Button className="btn-login" type="submit" variant="contained" size="large">
+                  Login
+                </Button>
+              </Grid>
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 

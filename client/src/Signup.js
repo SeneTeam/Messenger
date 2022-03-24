7d6068540689bf83224b338cc268cf39322a628c
minhchienef <minhchien.ef@gmail.com>
2022-03-24 10:19:37 +0700

    commit: fix all feedbacks

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Container,
   Grid,
@@ -11,20 +12,26 @@ import {
   FormHelperText,
   GridListTileBar,
 } from '@material-ui/core';
-import './Signup.scss';
 
-const styles = {
-  paperContainer: {
+export const useStyles = makeStyles(() => ({
+  root: {
     backgroundImage: `url(${'bg-img.png'}), linear-gradient(180deg, #3A8DFF 0%, #86B9FF 100%)`,
     backgroundRepeat: 'no-repeat',
     backgroundBlendMode: 'overlay',
     backgroundSize: 'cover',
     opacity: 0.85,
     position: 'relative',
+  },
+  header: {
+
+  },
+  fieldForm: {
+    height: `calc(100% - 86px)`,
   }
-};
+}));
 
 const Signup = ({ user, register }) => {
+  const classes = useStyles();
   const history = useHistory();
 
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -50,22 +57,22 @@ const Signup = ({ user, register }) => {
   }, [user, history]);
 
   return (
-    <Box className="Signup-Container" height="100vh" display="flex" flex="1" justifyContent="space-around">
+    <Box height="100vh" display="flex" flex="1" justifyContent="space-around">
       <Grid container justifyContent="center">
-        <Grid className="left-column width-100pc" item md={5} style={styles.paperContainer} justifyContent="center">
+        <Grid className={`${classes.root} left-column width-100pc`} item md={4} justifyContent="center">
           <div className="bubble-icon">
             <img src="bubble.svg" height={66} width={67} />
             <Typography>Converse with anyone with any language</Typography>
           </div>
         </Grid>
-        <Grid className="right-column" item md={7} direction="column">
+        <Grid className="right-column" item md={8} direction="column">
           <Box display="flex" flex="1" justifyContent="flex-end">
             <Typography>Already have an account?</Typography>
             <Link className="link" href="/login" to="/login">
               <Button variant="contained">Login</Button>
             </Link>
           </Box>
-          <Box display="flex" flex="1" justifyContent="center" style={{ height: `calc(100% - 1em - 24px` }}>
+          <Box className={classes.fieldForm} display="flex" flex="1" justifyContent="center">
             <form onSubmit={handleRegister}>
               <label className="banner">Create an account.</label>
               <Grid item md={12}>
